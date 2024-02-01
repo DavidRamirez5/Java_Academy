a5a303e617e003925c175b763fd7df6c0a4f3e80
David Ramirez <david.andrespr5@gmail.com>
2024-01-31 20:53:23 -0600

    Add files

---
+++ semanas/semana1/ejemplos/Abstraccion.java
@@ -0,0 +1,15 @@
+package semana1.ejemplos;
+
+public class Abstraccion {
+
+    public static void main(String[] args) {
+        // Se aplica a cosas que no sob tangibles.
+        // Modelar cosas que no existen.
+
+        // de lo no particular a lo particular.
+        // que es una operacion, no existe en el mundo real
+        // suma deriva de una operacion.
+        // la suma nace de esa abstracc
+    }
+
+}
---
+++ semanas/semana1/ejemplos/Generica.java
@@ -0,0 +1,19 @@
+package semana1.ejemplos;
+
+public class Generica {
+
+    public static void main(String[] args) {
+        // Los arrglos son de longitud fija.
+
+
+
+        // Estrucura de datos
+
+        // pasar de objeto a lista Lis.of(array) JAVA 9
+        // o Arrays.asList(array) JAVA 8 si se hace de esta manera la lista ya no puede crecer ni decrecer
+        // lista.get(element)
+
+
+
+    }
+}
